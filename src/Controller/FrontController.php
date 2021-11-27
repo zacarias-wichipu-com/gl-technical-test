c5ad16cb515f934b190b4f491e462b097740d151
Zacarías <zacarias@wichipu.com>
2021-11-27 12:27:30 +0100

    Add front dependecies.

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'front_')]
+class FrontController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+}
